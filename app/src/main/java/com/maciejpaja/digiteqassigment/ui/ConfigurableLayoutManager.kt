2988c2f195b68b85d0e6019cf2eb389d6193f95d
Maciej Paja <maciej.paja@redge.com>
2024-08-07 14:53:31 +0200

    Implemented scroll for recyclerview and fixed logic for next screens items

--- app/src/main/java/com/maciejpaja/digiteqassigment/ui/ConfigurableLayoutManager.kt
+++ app/src/main/java/com/maciejpaja/digiteqassigment/ui/ConfigurableLayoutManager.kt
@@ -1,10 +1,18 @@
 package com.maciejpaja.digiteqassigment.ui
 
+import android.view.ViewGroup
+import androidx.recyclerview.widget.OrientationHelper
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.RecyclerView.LayoutManager
+import kotlin.math.abs
+
 
 class ConfigurableLayoutManager(private val numberOfRows: Int, private val numberOfColumns: Int) :
     LayoutManager() {
+
+    private var orientationHelperForHorizontal: OrientationHelper? = null
+    private var offset = 0
+    private var lastItemEnd = 0
 
     override fun generateDefaultLayoutParams(): RecyclerView.LayoutParams {
         return RecyclerView.LayoutParams(
@@ -21,8 +29,9 @@ class ConfigurableLayoutManager(private val numberOfRows: Int, private val numbe
         if (itemCount == 0) return
         detachAndScrapAttachedViews(recycler)
 
-        var currentRow = 0
-        var currentColumn = 0
+        var currentRow = 1
+        var currentColumn = 1
+        var screenCount = 1
 
         for (position in 0 until itemCount) {
             val view = recycler.getViewForPosition(position)
@@ -36,21 +45,71 @@ class ConfigurableLayoutManager(private val numberOfRows: Int, private val numbe
             layoutParams.height = itemHeight
             view.layoutParams = layoutParams
 
+            if (position == numberOfColumns * numberOfRows * screenCount) {
+                screenCount++
+                currentRow = 1
+                currentColumn = 1
+            }
 
-            var left = itemWidth * currentColumn
-            var right = left + itemWidth
-            var top = if (currentRow == 0) 0 else itemHeight * currentRow
-            var bottom = top + (if (currentRow == 0) itemHeight else itemHeight * currentRow)
+            val left = itemWidth * (currentColumn - 1) + width * (screenCount -1)
+            val right = left + itemWidth
+            val top = if (currentRow == 1) 0 else itemHeight * (currentRow -1)
+            val bottom = top + (if (currentRow == 0) itemHeight else itemHeight * currentRow)
             currentColumn++
 
             if ((position + 1).rem(numberOfColumns) == 0 && position != 0) {
                 currentRow++
-                currentColumn = 0
+                currentColumn = 1
             }
-
 
             measureChild(view, width, width)
             layoutDecorated(view, left, top, right, bottom)
+
+            val lastItem = getChildAt(itemCount - 1)
+            lastItemEnd = lastItem?.run {
+                getDecoratedRight(lastItem) + (lastItem.layoutParams as ViewGroup.MarginLayoutParams).rightMargin
+            } ?: 0
+        }
+    }
+
+    override fun scrollHorizontallyBy(
+        dx: Int,
+        recycler: RecyclerView.Recycler?,
+        state: RecyclerView.State?
+    ): Int {
+        if (childCount == 0 || dx == 0) {
+            return 0
+        }
+
+        ensureLayoutState()
+        val scrollDirection = if (dx > 0) 1 else -1
+        val absoluteDx = abs(dx.toDouble()).toInt()
+        val horizontalOffset = calculateHorizontalOffsetForScroll(scrollDirection)
+        if (horizontalOffset < 0) {
+            return 0
+        }
+
+        val scrolled = if (absoluteDx > horizontalOffset) scrollDirection * horizontalOffset else dx
+        orientationHelperForHorizontal?.offsetChildren(-scrolled)
+        return scrolled
+    }
+
+    private fun calculateHorizontalOffsetForScroll(layoutDirection: Int) =
+        if (layoutDirection == 1) {
+            lastItemEnd + offset - width
+        } else {
+            -offset
+        }
+
+    override fun offsetChildrenHorizontal(dx: Int) {
+        super.offsetChildrenHorizontal(dx)
+        offset += dx
+    }
+
+    private fun ensureLayoutState() {
+        if (orientationHelperForHorizontal == null) {
+            orientationHelperForHorizontal =
+                OrientationHelper.createOrientationHelper(this, RecyclerView.HORIZONTAL)
         }
     }
 
